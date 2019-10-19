0bac44abad7d26d93c865a72b31a6a8434fa4e5f
willfenton <wfenton@ualberta.ca>
2019-10-19 11:28:57 -0600

    Search Results

--- canvas.js
+++ canvas.js
@@ -1,5 +1,7 @@
+// gets the canvas element
 var canvas = document.querySelector("canvas");
 
+// set canvas size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -8,18 +10,18 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext("2d");
 
 class Sun {
-    constructor() {
+    constructor(mass, radius, colour) {
+        // physics stuff (distance in m, mass in kg)
         this.phys_x = 0;
         this.phys_y = 0;
 
-        // this.mass = 100;
-        this.mass = 2 * (10 ** 30);
+        this.mass = mass;
 
+        // drawing stuff
         this.px = canvas.width / 2;
         this.py = canvas.height / 2;
 
-        this.radius = 30;
-
+        this.radius = radius;
         this.colour = "#FDB813";
     }
 
@@ -38,26 +40,30 @@ class Sun {
 }
 
 class Planet {
-    constructor(radius, colour, sun) {
-        this.phys_x = 1 * AU;
-        this.phys_y = 0;
+    constructor(phys_x, phys_y, mass, vx, vy, radius, colour, parent) {
+        this.parent = parent;
 
-        this.mass = 5.972 * (10 ** 24);
+        // physics stuff
+        this.phys_x = phys_x;
+        this.phys_y = phys_y;
+
+        this.mass = mass;
+
+        this.vx = vx;
+        this.vy = vy;
+        // this.vy = Math.sqrt(G * this.parent.mass / this.orbital_distance);
+
+        // drawing stuff
+        this.px = this.parent.px + (this.phys_x * pixels_per_m);
+        this.py = this.parent.py + (this.phys_y * pixels_per_m);
 
         this.radius = radius;
-        this.sun = sun;
-
-        this.px = this.sun.px + (this.phys_x * pixels_per_m);
-        this.py = this.sun.py + (this.phys_y * pixels_per_m);
-
-        this.vx = 0;
-        this.vy = Math.sqrt(G * this.sun.mass / this.orbital_distance);
         this.colour = colour;
     }
 
     draw() {
-        this.px = this.sun.px + (this.phys_x * pixels_per_m);
-        this.py = this.sun.py + (this.phys_y * pixels_per_m);
+        this.px = this.parent.px + (this.phys_x * pixels_per_m);
+        this.py = this.parent.py + (this.phys_y * pixels_per_m);
 
         c.strokeStyle = this.colour;
         c.fillStyle = this.colour;
@@ -75,15 +81,15 @@ class Planet {
     apply_physics() {
         this.phys_x += this.vx * time_scale;
         this.phys_y += this.vy * time_scale;
-        this.vx -= this.gravity * Math.cos(this.angle) * time_scale;
-        this.vy -= this.gravity * Math.sin(this.angle) * time_scale;
+        this.vx -= this.gravity_force * Math.cos(this.angle) * time_scale;
+        this.vy -= this.gravity_force * Math.sin(this.angle) * time_scale;
     }
 
     log() {
-        console.log("Sun", this.sun.px, this.sun.py);
+        console.log("Sun", this.parent.px, this.parent.py);
         console.log("Planet", this.px, this.py);
         console.log("Orbital Distance", this.orbital_distance / AU);
-        console.log("Gravity", this.gravity);
+        console.log("Gravity_force", this.gravity_force);
         console.log("Angle", this.angle);
     }
 
@@ -93,8 +99,8 @@ class Planet {
         return Math.sqrt(squared_distance);
     }
 
-    get gravity() {
-        return G * this.sun.mass / (this.orbital_distance ** 2);
+    get gravity_force() {
+        return G * this.parent.mass / (this.orbital_distance ** 2);
     }
 
     get angle() {
@@ -102,23 +108,70 @@ class Planet {
     }
 }
 
+function new_planet_radius(mass, angle, orbital_radius, radius, colour, parent) {
+    // phys_x, phys_y, mass, vx, vy, radius, colour, parent)
+    var phys_x = orbital_radius * Math.cos(angle);
+    var phys_y = orbital_radius * Math.sin(angle);
+
+    var v = Math.sqrt(G * parent.mass / orbital_radius);
+    var vx = v * Math.cos((Math.PI / 2) - angle);
+    var vy = -v * Math.sin((Math.PI / 2) - angle);
+    // var vx = 0;
+    // var vy = 0;
+
+    var planet = new Planet(phys_x, phys_y, mass, vx, vy, radius, colour, parent);
+    return planet;
+}
+
+function new_planet_velocity(mass, angle, velocity, radius, colour, parent) {
+    return NaN;
+}
+
+// physics constancts
 const time_scale = 100000;
 
 const G = 6.67408 * (10 ** -11);
 const AU = 149597870700;    // meters
 
-var width_m = 6 * AU;
+// canvas scaling
+var width_m = 12 * AU;
 var height_m = (canvas.height / canvas.width) * width_m;
 
 var pixels_per_m = canvas.width / width_m;
 
+// create objects
 var objects = [];
 
-var sun = new Sun();
+var sun_mass = 2 * (10 ** 30);
+var sun_radius = 30;
+var sun_colour = "#FDB813";
+
+var sun = new Sun(sun_mass, sun_radius, sun_colour);
 objects.push(sun);
 
-var planet = new Planet(10, "#6c3bd4", sun);
-objects.push(planet);
+// var planet = new Planet(10, "#6c3bd4", sun);
+// objects.push(planet);
+
+// var planet_mass = 5.972 * (10 ** 24);
+// var planet_angle = 0;
+// var planet_orbital_radius = 2 * AU;
+// var planet_radius = 10;
+// var planet_colour = "#6c3bd4";
+
+var num_planets = 100;
+
+// create planets
+for (var i = 0; i < num_planets; i++) {
+    var mass = 5.972 * (10 ** 24);
+    var angle = (Math.random() * 2) * Math.PI;
+    var orbital_radius = (Math.round(Math.random() * 5) + 1) * AU;
+    
+    var radius = Math.round(Math.random() * 10) + 5;
+    var colour = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+
+    var planet = new_planet_radius(mass, angle, orbital_radius, radius, colour, sun);
+    objects.push(planet);
+}
 
 function animate() {
     // call animate in a loop for each frame
@@ -138,14 +191,16 @@ function drawGrid() {
     // draw a line
     for (let i = 0; i <= width_m; i += AU) {
         c.beginPath();
-        c.strokeStyle = "#fa34a3";
+        // c.strokeStyle = "#fa34a3";
+        c.strokeStyle = "rgba(255, 255, 255, 0.3)"
         c.moveTo(i * pixels_per_m, 0);
         c.lineTo(i * pixels_per_m, canvas.height);
         c.stroke();
     }
     for (let i = 0; i <= height_m; i += AU) {
         c.beginPath();
-        c.strokeStyle = "#fa34a3";
+        // c.strokeStyle = "#fa34a3";
+        c.strokeStyle = "rgba(255, 255, 255, 0.3)"
         c.moveTo(0, i * pixels_per_m);
         c.lineTo(canvas.width, i * pixels_per_m);
         c.stroke();
